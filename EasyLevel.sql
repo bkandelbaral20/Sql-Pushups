65ab4ad063da76034a0db40e082939e4e2c1c5d3
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-12-20 20:04:25 -0600

    returning city names starts with vowels

--- EasyLevel.sql
+++ EasyLevel.sql
@@ -66,3 +66,24 @@ SELECT City AND STATE from Station;
 SELECT DISTINCT CITY FROM STATION
 WHERE id % 2 = 0
 ORDER BY CITY;
+
+-- 9.
+-- Find the difference between the total number of CITY entries in the table
+-- and the number of distinct CITY entries in the table.
+-- For example, if there are three records in the table with CITY values 'New York', 'New York', 'Bengalaru',
+-- there are 2 different city names: 'New York' and 'Bengalaru'. The query returns 1 , because
+-- total numbers of record - number of unique city name = 3-2 = 1
+
+
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION ;
+
+-- 10.
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION.
+-- Your result cannot contain duplicates.
+
+SELECT DISTINCT City From STATION
+WHERE  city  LIKE 'A%'
+   or  city  LIKE 'E%'
+   or city  LIKE 'I%'
+   or city  LIKE 'O%'
+   or city  LIKE 'U%';
