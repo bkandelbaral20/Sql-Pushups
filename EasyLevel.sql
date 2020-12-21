4b5aeefcf6c3b3f673a5325b43efa5b651e969fd
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-12-21 12:36:25 -0600

    not returning city names ending with vowels

--- EasyLevel.sql
+++ EasyLevel.sql
@@ -1,4 +1,3 @@
-
 -- 1.
 
 -- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
@@ -11,38 +10,46 @@
 -- District     varchar
 -- Population   number
 
-Select * from City
-where population > 100000 AND CountryCode='USA';
+Select *
+from City
+where population > 100000
+  AND CountryCode = 'USA';
 
 -- 2.
 -- Query the NAME field for all American cities in the CITY table with populations larger than 120000.
 -- The CountryCode for America is USA.
 
-SELECT Name from City
-where population > 120000 AND CountryCode='USA';
+SELECT Name
+from City
+where population > 120000
+  AND CountryCode = 'USA';
 
 -- 3.
 -- Query all columns (attributes) for every row in the CITY table.
 
-SELECT * from City;
+SELECT *
+from City;
 
 -- 4.
 -- Query all columns for a city in CITY with the ID 1661.
 
-SELECT * from City
-where id= 1661;
+SELECT *
+from City
+where id = 1661;
 
 -- 5.
 -- Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
 
-SELECT * from City
-where CountryCode='JPN';
+SELECT *
+from City
+where CountryCode = 'JPN';
 
 -- 6.
 -- Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
 
-SELECT name from City
-where CountryCode='JPN';
+SELECT name
+from City
+where CountryCode = 'JPN';
 
 -- 7.
 -- Query a list of CITY and STATE from the STATION table.
@@ -57,13 +64,15 @@ where CountryCode='JPN';
 
 -- where LAT_N is the northern latitude and LONG_W is the western longitude.
 
-SELECT City AND STATE from Station;
+SELECT City AND STATE
+from Station;
 
 -- 8.
 -- Query a list of CITY names from STATION for cities that have an even ID number.
 -- Print the results in any order, but exclude duplicates from the answer.
 
-SELECT DISTINCT CITY FROM STATION
+SELECT DISTINCT CITY
+FROM STATION
 WHERE id % 2 = 0
 ORDER BY CITY;
 
@@ -75,43 +84,46 @@ ORDER BY CITY;
 -- total numbers of record - number of unique city name = 3-2 = 1
 
 
-SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION ;
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION;
 
 -- 10.
 -- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION.
 -- Your result cannot contain duplicates.
 
-SELECT DISTINCT City From STATION
-WHERE  city  LIKE 'A%'
-   or  city  LIKE 'E%'
-   or city  LIKE 'I%'
-   or city  LIKE 'O%'
-   or city  LIKE 'U%';
+SELECT DISTINCT City
+From STATION
+WHERE city LIKE 'A%'
+   or city LIKE 'E%'
+   or city LIKE 'I%'
+   or city LIKE 'O%'
+   or city LIKE 'U%';
 
 
 -- 11.
 -- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
 
-SELECT DISTINCT City From STATION
-WHERE  city  LIKE '%A'
-   or  city  LIKE '%E'
-   or city  LIKE '%I'
-   or city  LIKE '%O'
-   or city  LIKE '%U';
+SELECT DISTINCT City
+From STATION
+WHERE city LIKE '%A'
+   or city LIKE '%E'
+   or city LIKE '%I'
+   or city LIKE '%O'
+   or city LIKE '%U';
 
 -- 12.
 -- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u)
 -- as both their first and last characters. Your result cannot contain duplicates.
 
-SELECT distinct CITY FROM STATION
-where ( CITY LIKE 'a%'
+SELECT distinct CITY
+FROM STATION
+where (CITY LIKE 'a%'
     OR CITY LIKE 'e%'
     OR CITY LIKE 'i%'
     OR CITY LIKE 'o%'
     OR CITY LIKE 'u%'
     )
-  AND
-    ( CITY LIKE '%a'
+  AND (CITY LIKE '%a'
     OR CITY LIKE '%e'
     OR CITY LIKE '%i'
     OR CITY LIKE '%o'
@@ -121,7 +133,8 @@ where ( CITY LIKE 'a%'
 -- 13.
 -- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
 
-SELECT DISTINCT City from Station
+SELECT DISTINCT City
+from Station
 where City Not LIKE 'a%'
    OR CITY Not LIKE 'e%'
    OR CITY Not LIKE 'i%'
@@ -130,9 +143,64 @@ where City Not LIKE 'a%'
 
 -- 14.
 -- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
-SELECT DISTINCT City from Station
+SELECT DISTINCT City
+from Station
 where City Not LIKE '%a'
    OR CITY Not LIKE '%e'
    OR CITY Not LIKE '%i'
    OR CITY Not LIKE '%o'
    OR CITY Not LIKE '%u';
+
+-- 15.
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels.
+-- Your result cannot contain duplicates.
+
+SELECT DISTINCT City
+from Station
+where (City Not LIKE 'a%'
+    OR CITY Not LIKE 'e%'
+    OR CITY Not LIKE 'i%'
+    OR CITY Not LIKE 'o%'
+    OR CITY Not LIKE 'u%')
+   Or (City Not LIKE '%a'
+    OR CITY Not LIKE '%e'
+    OR CITY Not LIKE '%i'
+    OR CITY Not LIKE '%o'
+    OR CITY Not LIKE '%u');
+
+
+-- 16.
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels.
+-- Your result cannot contain duplicates.
+
+SELECT DISTINCT City
+from Station
+where (City Not LIKE 'a%'
+    OR CITY Not LIKE 'e%'
+    OR CITY Not LIKE 'i%'
+    OR CITY Not LIKE 'o%'
+    OR CITY Not LIKE 'u%')
+  AND (City Not LIKE '%a'
+    OR CITY Not LIKE '%e'
+    OR CITY Not LIKE '%i'
+    OR CITY Not LIKE '%o'
+    OR CITY Not LIKE '%u');
+
+-- 17.
+-- Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three
+-- characters of each name. If two or more students both have names ending in the same last three
+-- characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+-- Input Format:
+-- The STUDENTS table is described as follows:
+
+--  COLUMN    TYPE
+--  Id        INTEGER
+--  Name      String
+--  Marks     INTEGER
+
+-- The Name column only contains uppercase (A-Z) and lowercase (a-z) letters.
+
+SELECT Name FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name, 3), ASC ID;
