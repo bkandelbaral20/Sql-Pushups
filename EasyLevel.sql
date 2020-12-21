08673d984630dae1195d74afd633c8007f8717e4
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-12-21 11:14:34 -0600

    returning city names starting and  ending with vowels

--- EasyLevel.sql
+++ EasyLevel.sql
@@ -98,3 +98,22 @@ WHERE  city  LIKE '%A'
    or city  LIKE '%I'
    or city  LIKE '%O'
    or city  LIKE '%U';
+
+-- 12.
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u)
+-- as both their first and last characters. Your result cannot contain duplicates.
+
+SELECT distinct CITY FROM STATION
+where ( CITY LIKE 'a%'
+    OR CITY LIKE 'e%'
+    OR CITY LIKE 'i%'
+    OR CITY LIKE 'o%'
+    OR CITY LIKE 'u%'
+    )
+  AND
+    ( CITY LIKE '%a'
+    OR CITY LIKE '%e'
+    OR CITY LIKE '%i'
+    OR CITY LIKE '%o'
+    OR CITY LIKE '%u'
+    );
