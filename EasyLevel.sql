2b66d7524d2c63fe378635acd17e1c9526f13661
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-12-21 11:20:50 -0600

    not returning city names ending with vowels

--- EasyLevel.sql
+++ EasyLevel.sql
@@ -127,3 +127,12 @@ where City Not LIKE 'a%'
    OR CITY Not LIKE 'i%'
    OR CITY Not LIKE 'o%'
    OR CITY Not LIKE 'u%';
+
+-- 14.
+-- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT City from Station
+where City Not LIKE '%a'
+   OR CITY Not LIKE '%e'
+   OR CITY Not LIKE '%i'
+   OR CITY Not LIKE '%o'
+   OR CITY Not LIKE '%u';
